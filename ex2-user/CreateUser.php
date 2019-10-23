52de6a89803bd1eeecf7a1491ed5bbf6509d492c
Connie <constanceli@ku.edu>
2019-10-23 12:56:34 -0500

    fix(ex2): checking for existing user

--- ex2-user/CreateUser.php
+++ ex2-user/CreateUser.php
@@ -1,5 +1,7 @@
 <?php
     /* this section is based on the example in the lab spec */
+    error_reporting(E_ALL);
+    ini_set("display_errors", 1);
     $mysqli = new mysqli("mysql.eecs.ku.edu", "constanceli", "aph7Yui7", "constanceli");
 
     if ($mysqli->connect_errno) {
@@ -8,31 +10,22 @@
     }
 
     $username = $_POST["username"];
-    $queryExists = "SELECT user_id FROM Users WHERE user_id=" . $_POST["username"] . ");";
+    $queryExists = "SELECT user_id FROM Users WHERE user_id='$username'";
+
     if($result = $mysqli->query($queryExists));
     {
-        echo "result true";
         $userExists = false;
         while($row = $result->fetch_assoc())
         {
-            echo $row["user_id"];
             if($row["user_id"] == $username)
             {
                 $userExists = true;
-                echo $userExists;
             }
         }
         if($userExists)
         {
             echo "Sorry, that username already exists.\n";
         }
-
-    //    $numRows = $result->num_rows;
-    //    echo '$numRows = ' . $numRows . "\n";
-    //    if($numRows != 0)
-    //    {
-    //        echo "Sorry, that username already exists.\n";
-    //    }
         else
         {
             $queryInsert = "INSERT INTO Users (user_id) VALUES ('" . $_POST["username"] . "');";
