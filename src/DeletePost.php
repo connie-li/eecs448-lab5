67df15d4799c05297190274430c597986a99171f
Connie <constanceli@ku.edu>
2019-10-28 04:02:51 -0500

    fix(ex7): deletion & clean up HTML

--- src/DeletePost.php
+++ src/DeletePost.php
@@ -34,6 +34,19 @@ function deleteChecked() {
 		exit();
 	}
 
+	echo '<!DOCTYPE html>
+	<html>
+		<head>
+			<meta charset="utf-8">
+			<title>Exercise 7: Delete Posts - Lab 5, EECS 448</title>
+			<link rel="stylesheet" type="text/css" href="../styles/styles.css">
+		</head>
+		<body>
+			<div>Exercise 7: Delete Posts - Lab 5, EECS 448</div>
+			<div id="content">
+				<h1 class="centered">Confirmation of Deletion(s)</h1>
+				<div class="inner-content">
+				<p>The following Post IDs were deleted:</p><ul>';
 	foreach($_POST as $key => $value)
 	{
 		if(isset($key) && $key != "submit")
@@ -41,18 +54,20 @@ function deleteChecked() {
 			$query = "DELETE FROM Posts WHERE post_id='$_POST[$key]';";
 			if($result = $mysqli->query($query))
 			{
-				echo "Deleted post $key.";
+				echo "<li>$key</li>";
 			}
 			else
 			{
-				echo "Error deleting ($key)";
+				echo "<div>Error deleting ($key)</div>";
 			}
 		}
 	}
 	$mysqli->close();
+	
+	echo '</ul></div></div></body></html>';
 }
 
-if(isset($_POST))
+if(isset($_POST["submit"]))
 {
 	deleteChecked();
 }
