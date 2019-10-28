08991c0946a9211dc5e2c51d867230da67ed581e
Connie <constanceli@ku.edu>
2019-10-28 01:51:55 -0500

    feat(ex6): get the user's posts & display them

--- src/ViewUserPosts.php
+++ src/ViewUserPosts.php
@@ -26,6 +26,28 @@ function getUsers() {
 	
 }
 
+	error_reporting(E_ALL);
+	ini_set("display_errors", 1);
+
+	$mysqli = new mysqli("mysql.eecs.ku.edu", "constanceli", "aph7Yui7", "constanceli");
+	if ($mysqli->connect_errno) {
+		printf("Connect failed: %s\n", $mysqli->connect_error);
+		exit();
+	}
+	
+	$query = "SELECT * from Posts WHERE author_id='$user';";
+	if($result = $mysqli->query($query))
+	{
+		while($row = $result->fetch_assoc())
+		{
+			echo '<tr><td>' . $row["post_id"] . '</td><td>' . $row["content"] . '</td></tr>';
+		}
+	}
+	else
+	{
+		echo "Unable to query the server.";
+	}
+	$mysqli->close();
 
 
 
