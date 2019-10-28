cb9817e090b96a14ca1657f7e842d7fb9e0053e7
Connie <constanceli@ku.edu>
2019-10-28 00:45:24 -0500

    feat(ex6): populate dropdown list with users

---
+++ src/ViewUserPosts.php
@@ -0,0 +1,33 @@
+<?php
+
+function getUsers() {
+	error_reporting(E_ALL);
+	ini_set("display_errors", 1);
+
+	$mysqli = new mysqli("mysql.eecs.ku.edu", "constanceli", "aph7Yui7", "constanceli");
+	if ($mysqli->connect_errno) {
+		printf("Connect failed: %s\n", $mysqli->connect_error);
+		exit();
+	}
+	
+	$query = "SELECT * FROM Users;";
+	if($result = $mysqli->query($query))
+	{
+		while($row = $result->fetch_assoc())
+		{
+			echo '<option value="' . $row["user_id"] . '">' . $row["user_id"] . '</option>';
+		}
+	}
+	else
+	{
+		echo "Unable to query the server.";
+	}
+	$mysqli->close();
+	
+}
+
+
+
+
+
+?>
